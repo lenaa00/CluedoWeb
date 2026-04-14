3fee79184afb2e676b90e056e34fac65494cc632
lenaa00 <lena.lucedubas@ynov.com>
2026-04-14 12:09:39 +0200

    test

--- obj/Debug/net10.0/CluedoWeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/CluedoWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CluedoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70d68585bc6bf8582340a883f2c682beedc7102d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a402285f2ce80d7386b44c392557674813c5c13a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CluedoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CluedoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
