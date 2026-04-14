8002e35bad3680079f63f51dfa1779d2c19b09aa
lenaa00 <lena.lucedubas@ynov.com>
2026-04-14 10:05:05 +0200

    initialisation

---
+++ Pages/Regles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CluedoWeb.Pages;
+
+public class ReglesModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
